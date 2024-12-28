20bcbd1596e56fffe963a03fa2aff27321cd517d
Nithin sukumar <nithinsukumar200@gmail.com>
2024-12-28 23:55:20 +0530

    styling completed

--- src/product.jsx
+++ src/product.jsx
@@ -2,19 +2,24 @@ import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import  "./css/product.css" 
 function product(props) {
   return (
     <div>
-        <Card style={{ width: '18rem' }}>
+        <Card>
+          <div className="image-box">
       <Card.Img variant="top" src={props.productData.image}/>
+      </div>
       <Card.Body>
         <Card.Title>{props.productData.title}</Card.Title>
         <Card.Text>
           <p>Description{props.productData.description}</p>
-          <p>${props.productData.price}</p>
+          <p className='price'>${props.productData.price}</p>
         </Card.Text>
+        <div className="button-box">
         <Button variant="primary">Add to cart</Button>
         <Button variant="warning">View</Button>
+        </div>
       </Card.Body>
     </Card>
     </div>
